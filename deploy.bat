826cb8aa77a1f21f0b68a9274c3b4b790f5be441
elleriumsoft <enigmaex@mail.ru>
2017-01-25 00:33:04 +0400

    1 коммит

---
+++ deploy.bat
@@ -0,0 +1,2 @@
+call mvn clean install wildfly:deploy
+pause
---
+++ src/main/java/TestServ.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@SuppressWarnings("serial")
+@WebServlet("/Laba3")
+public class TestServ extends HttpServlet
+{
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        resp.setContentType("text/html;charset=utf-8");
+
+        PrintWriter pw = resp.getWriter();
+        pw.println("<H1>Ура заработало</H1>");
+        pw.flush();
+    }
+}
