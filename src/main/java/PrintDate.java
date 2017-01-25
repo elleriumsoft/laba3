2764f82da92086c3edd713e4717757139e9329ce
enigmaex <enigmaex@mail.ru>
2017-01-25 16:48:35 +0400

    second

--- src/main/java/PrintDate.java
+++ src/main/java/PrintDate.java
@@ -5,18 +5,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 
-@SuppressWarnings("serial")
-@WebServlet("/Laba3")
-public class TestServ extends HttpServlet
+@WebServlet("/PrintDate")
+public class PrintDate extends HttpServlet
 {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
         resp.setContentType("text/html;charset=utf-8");
 
         PrintWriter pw = resp.getWriter();
-        pw.println("<H1><b>Собственно это тест</b></H1>");
-        pw.println("<a href=\"/PrintDate\">Здесь 2я страница с выводом даты</a>");
+        long currentTime = System.currentTimeMillis();
+        SimpleDateFormat formating = new SimpleDateFormat("dd.MM.YYYY    HH:mm:ss");
+        String dateTime = formating.format(currentTime);
+        pw.println("<H1><b>Текущее время:</b></H1>");
+        pw.println("<H2>" + dateTime + "</H2>");
         pw.flush();
     }
 }
